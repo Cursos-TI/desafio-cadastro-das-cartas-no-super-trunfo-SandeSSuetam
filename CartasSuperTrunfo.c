a19389dfdc3e9f670abd93079431617f567276bb
SandeSSuetam <bypassfreelinux@gmail.com>
2025-04-22 05:08:50 -0300

    Update CartasSuperTrunfo.c
    adicionado a logica de escolher ataque

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     // Variáveis da carta 1
@@ -10,7 +11,7 @@ int main() {
 
     // Variáveis da carta 2
     char codigo1[4];
-    int populacao1;
+    unsigned long int populacao1;
     float area1;
     float pib1;
     int pontoturistico1;
@@ -22,8 +23,11 @@ int main() {
     float media1;
     int superPoder;
     int superpoder1;
-    int superPoderCaluculo = pib + populacao + pontoturistico + meida  / densidade;
-    int superPoderCaluculo1 = pib1 + populacao1 + pontoturistico1 + media1  / densidade1;
+    
+    /* Cálculo do super poder estava antes da leitura dos dados, 
+       movido para depois da leitura para funcionar corretamente */
+    // Variáveis de escolhas 
+    int opcao;
     /* Variáveis de resultado de vencedor 
     int resultado = pib > pib1;
     int resultadoPT = pontoturistico > pontoturistico1;
@@ -56,13 +60,15 @@ int main() {
     printf("Informe o número de pontos turísticos: ");
     scanf("%d", &pontoturistico);
 
-    // Exibição da carta 1
+    /* Exibição da carta 1
     printf("\n--- Dados da Carta 1 ---\n");
     printf("Código: %s\n", codigo);
     printf("População: %lu\n", populacao);
     printf("Área: %.2f km²\n", area);
     printf("PIB: %.2f bilhões\n", pib);
     printf("Pontos turísticos: %d\n", pontoturistico);
+    */
+
 
     // Carta 2 
     printf("\n--- Carta da Cidade 2 ---\n");
@@ -70,7 +76,7 @@ int main() {
     scanf("%s", codigo1);
 
     printf("Informe a população: ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Informe a área: ");
     scanf("%f", &area1);
@@ -81,30 +87,90 @@ int main() {
     printf("Informe o número de pontos turísticos: ");
     scanf("%d", &pontoturistico1);
 
-    // Exibição da carta 2
+    /* Exibição da carta 2
     printf("\n--- Dados da Carta 2 ---\n");
     printf("Código: %s\n", codigo1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões\n", pib1);
     printf("Pontos turísticos: %d\n", pontoturistico1);
+    */
 
     // Cálculo das médias
     densidade = populacao / area;
     densidade1 = populacao1 / area1;
     meida = (float)(pib / populacao);
-    superPoder = superPoderCaluculo > superPoderCaluculo1;
     
     printf("\nPIB per capita da Cidade 1 R$: %.2f\n", meida);
 
     media1 = (float)(pib1 / populacao1);
     printf("PIB per capita da Cidade 2 R$: %.2f\n", media1);
 
+    // Cálculo do super poder (feito depois da leitura)
+    int superPoderCaluculo = pib + populacao + pontoturistico + meida / densidade;
+    int superPoderCaluculo1 = pib1 + populacao1 + pontoturistico1 + media1 / densidade1;
+
+    superPoder = superPoderCaluculo;
+    superpoder1 = superPoderCaluculo1;
+
     // Resultado do vencedor com 1 para verdadeiro e 0 para falso 
     /* printf("\nResultado 1: sua carta venceu (valor maior).\n");
     printf("Resultado 0: sua carta perdeu (valor menor).\n"); */
     printf("\033[1;31mRESULTADO DA PARTIDA\033[0m\n");
+    printf("Escolha qual vai ser os comparadores.\n");
+    printf("1. População.\n");
+    printf("2. Área.\n");
+    printf("3. PIB.\n");
+    printf("4. Número de pontos turísticos.\n");
+    printf("5. Super Poder.\n");
+    scanf("%d", &opcao);
+    switch (opcao)
+    {
+    case 1 :
+        if (populacao > populacao1){
+            printf("\nPopulação da carta 1 ganhou");
+        }else{
+            printf("\nPopulação da carta 2 ganhou \n");
+        }
+        
+        break;
+    case 2 :
+        if (area > area1)
+        {
+            printf("Área da carta 1 ganhou.\n");
+        }else{
+            printf("Área da carta 2 ganhou.\n");
+        }
+        break;
+    case 3 :
+        if (pib > pib1)
+        {
+            printf("PIB da carta 1 ganhou.\n");
+        }else{
+            printf("PIB da carta 2 ganhou.\n");
+        }
+        break;
+    case 4 :
+        if (pontoturistico > pontoturistico1)
+        {
+            printf("Ponto turístico da carta 1 ganhou.\n");
+        }else{
+            printf("Ponto turístico da carta 2 ganhou.\n");
+        }
+        break;
+    case 5 : 
+       if (superPoder > superpoder1)
+       {
+        printf("Super Poder da carta 1 ganhou.\n");
+       }else{
+        printf("Super Poder da carta 2 ganhou.\n");
+       }
+       break;
+    default:
+        break;
+    }
 
+/*
     if (meida > media1) {
         printf("PIB PER CAPITA DA CARTA 1 - Você ganhou\n");
     } else {
@@ -129,10 +195,10 @@ int main() {
         printf("Densidade ganhadora foi a Carta 1\n");
     }
     if (superPoderCaluculo > superPoderCaluculo1){
-        printf("SUOOER TRUNFO O GANHADOR FINAL FOI VOCE CARTA 2 DE CODIGO : %s ", codigo1);
+        printf("SUPER TRUNFO - O GANHADOR FINAL FOI VOCÊ, CARTA 1 DE CÓDIGO: %s\n", codigo);
     }else{
-        printf("SUEPER TRUNDO O GANHADOR FINAL VOCE CARTA 1 CODIGO : %s", codigo);
+        printf("SUPER TRUNFO - O GANHADOR FINAL FOI VOCÊ, CARTA 2 DE CÓDIGO: %s\n", codigo1);
     }
-
+*/
     return 0;
 }
